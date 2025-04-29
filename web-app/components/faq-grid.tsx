20687883ea75d92d8c80bfa4c9e2b1bc8962518a
emiliano <emiliano_sanchez@criptext.com>
2025-04-29 15:38:56 -0600

    feat: implement FAQ grid with data fetching

--- web-app/components/faq-grid.tsx
+++ web-app/components/faq-grid.tsx
@@ -1,28 +1,43 @@
-"use client"
+import FaqItem from "./faq-item";
+// import { faqData } from "@/lib/data";
+import type { Faq } from "@/types/schema";
 
-import { useState } from "react"
-import FaqItem from "./faq-item"
-import { faqData } from "@/lib/data"
 
-export default function FaqGrid() {
-  const [openItems, setOpenItems] = useState<number[]>(faqData.map((item) => item.id))
+async function getFaqs() {
+  const apiUrl = process.env.API_URL; // → "http://backend:8000"
 
-  const toggleItem = (id: number) => {
-    setOpenItems((prev) => (prev.includes(id) ? prev.filter((itemId) => itemId !== id) : [...prev, id]))
+  const res = await fetch(`${apiUrl}/api/faqs?limit=20&randomize=false`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }); 
+ 
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error('Failed to fetch data', {
+      cause: new Error(errorData.message),
+    })
   }
+  return res.json()
+}
+ 
+
+export default async function FaqGrid() {
+  const faqs: Faq[] = await getFaqs();
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-12 gap-y-0 mt-6">
-      {faqData.map((item) => (
+      {faqs.map((item) => (
         <FaqItem
           key={item.id}
           id={item.id}
           question={item.question}
           answer={item.answer}
-          isOpen={openItems.includes(item.id)}
-          onToggle={() => toggleItem(item.id)}
+          // isOpen={openItems.includes(item.id)}
+          // onToggle={() => toggleItem(item.id)}
         />
       ))}
     </div>
-  )
+  );
 }
