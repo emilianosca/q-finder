b84841d7928fcdd43a74cf34979ae87775aaef37
emiliano <emiliano_sanchez@criptext.com>
2025-04-29 10:56:35 -0600

    update nextjs config, deleted api overwrite

--- web-app/next.config.mjs
+++ web-app/next.config.mjs
@@ -2,3 +2,22 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
+
+// const API_HOST = process.env.API_URL || "http://backend:8000"
+
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination: `${API_HOST}/api/:path*`,
+//       },
+//     ]
+//   },
+// }
+
+// export default nextConfig
