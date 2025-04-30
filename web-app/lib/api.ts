204d40155fdab0417ba4a0d5b410810e2dcd8b63
emiliano <emiliano_sanchez@criptext.com>
2025-04-30 10:44:52 -0600

    added next base url (for sitemap)

--- web-app/lib/api.ts
+++ web-app/lib/api.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL; // → "http://backend:8000"
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://backend:8000";
+export const NEXT_BASE_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || "http://frontend:3000";
